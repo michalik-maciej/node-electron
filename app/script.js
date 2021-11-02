ca1d331683d87d2d9b626cc08dee92a086d1448f
Maciej Michalik <halniak24@gmail.com>
2021-11-02 08:20:53 +0100

    Add error document undefined

--- app/script.js
+++ app/script.js
@@ -1,25 +1,7 @@
-import React from 'react';
-import { render } from 'react-dom';
+const os = require('os');
 
-class App extends React.Component {
+const osPlatform = document.querySelector('#osPlatform');
+const osArchitecture = document.querySelector('#osArchitecture');
 
-  render() {
-    return (
-      <div>
-        <h1>Protect your eyes</h1>
-        <p>According to optometrists in order to save your eyes, you should follow the 20/20/20. It means you should to rest your eyes every 20 minutes for 20 seconds by looking more than 20 feet away.</p>
-        <p>This app will help you track your time and inform you when it's time to rest.</p>
-        <img src="./images/work.png" />
-        <img src="./images/rest.png" />
-        <div className="timer">
-          18:23
-        </div>
-        <button className="btn">Start</button>
-        <button className="btn">Stop</button>
-        <button className="btn btn-close">X</button>
-      </div>
-    )
-  }
-};
-console.log('document.querySelector', document.querySelector('#app'));
-render(<App />, document.querySelector('#app'));
+osPlatform.innerHTML = `Platform: <strong>${os.platform()}</strong>`;
+osArchitecture.innerHTML = `Platform: <strong>${os.arch()}</strong>`;
--- app1/script.js
+++
@@ -1,7 +0,0 @@
-const os = require('os');
-
-const osPlatform = document.querySelector('#osPlatform');
-const osArchitecture = document.querySelector('#osArchitecture');
-
-osPlatform.innerHTML = `Platform: <strong>${os.platform()}</strong>`;
-osArchitecture.innerHTML = `Platform: <strong>${os.arch()}</strong>`;
